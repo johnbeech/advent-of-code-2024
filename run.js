450ad5f1b20df900f78861abcdd2285656c03605
John Beech <johnbeech.github@mkv25.net>
2024-12-01 10:33:46 +0000

    Create commands to help with commit messages

--- run.js
+++ run.js
@@ -1,25 +1,53 @@
-const solutionId = process.argv[2]
+const { run } = require('promise-path')
 
-async function start () {
-  try {
-    await runSolution()
-  } catch (ex) {
-    if (!solutionId) {
-      console.error('No solution ID provided; please re-run with an argument, e.g.: npm start day1, or: node run day1')
-    } else {
-      await copyTemplate()
+const args = process.argv.slice(2)
+
+const commands = {
+  runSolution: async (solutionId) => {
+    try {
+      return require(`./solutions/${solutionId}/solution.js`)
+    } catch (ex) {
+      if (!solutionId) {
+        console.error('No solution ID provided; please re-run with an argument, e.g.: npm start day1, or: node run day1')
+      } else {
+        await copyTemplate()
+      }
     }
+  },
+  '--first-star': async (solutionId) => {
+    const command = `git commit -am "Solve ${solutionId} for first star"`
+    console.log('Running:', command)
+    await run(command)
+  },
+  '--second-star': async (solutionId) => {
+    const command = `git commit -am "Solve ${solutionId} for second star"`
+    console.log('Running:', command)
+    await run(command)
   }
 }
 
-function runSolution () {
-  return require(`./solutions/${solutionId}/solution.js`)
+async function start () {
+  const solutionId = args.pop()
+  const commandKey = args.pop()
+  const command = commands[commandKey] ?? commands.runSolution
+
+  if (!solutionId?.startsWith('day')) {
+    console.error('Invalid solution ID provided; please re-run with an argument, e.g.: npm start day1, or: node run day1')
+    return
+  }
+
+  try {
+    await command(solutionId)
+  } catch (ex) {
+    console.error(`Unable to process command for '${solutionId}': ${ex?.message}`, { commandKey, solutionId })
+  }
 }
 
-async function copyTemplate () {
+async function copyTemplate (solutionId) {
   try {
     await require('./copy-template.js')
-    await runSolution()
+    await commands.runSolution(solutionId)
+    await run(`git commit -am "Create template for ${solutionId}"`)
   } catch (ex) {
     console.error(`Unable to run solution for '${solutionId}': ${ex}`, ex.stack)
   }
