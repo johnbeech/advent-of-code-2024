f50caf8cd0392066d6e182213b6f113940e24252
John Beech <johnbeech.github@mkv25.net>
2024-12-02 08:53:46 +0000

    "Solve day2 for second star"

--- solutions/day2/solution.js
+++ solutions/day2/solution.js
@@ -14,49 +14,82 @@ async function solveForFirstStar (input) {
   const lines = input.split('\n')
   const levels = lines.map(line => line.trim().split(' ').map(Number))
 
-  const levelsExpanded = levels.map(level => {
-    const original = [...level]
-    const sorted = level.sort((a, b) => a - b)
-    const isAscending = sorted.join('') === original.join('')
-    const isDescending = sorted.slice().reverse().join('') === original.join('')
-    const maxDiff = original.reduce((acc, value, index) => {
-      const nextValue = original[index + 1]
-      if (nextValue === undefined) return acc
-      const diff = Math.abs(value - nextValue)
-      return diff > acc ? diff : acc
-    }, 0)
-    const minDiff = original.reduce((acc, value, index) => {
-      const nextValue = original[index + 1]
-      if (nextValue === undefined) return acc
-      const diff = Math.abs(nextValue - value)
-      return diff < acc ? diff : acc
-    }, Infinity)
-    const isDiffSafe = maxDiff <= 3 && minDiff >= 1
-    const isLevelSafe = (isAscending || isDescending) && isDiffSafe
-    return {
-      original: original.join(' '),
-      sorted: sorted.join(' '),
-      reverse: sorted.slice().reverse().join(' '),
-      isAscending,
-      isDescending,
-      minDiff,
-      maxDiff,
-      isDiffSafe,
-      isLevelSafe
-    }
-  })
+  const scoredLevels = levels.map(scoreLevel)
 
-  const numberOfSafeLevels = levelsExpanded.filter(level => level.isLevelSafe).length
+  const numberOfSafeLevels = scoredLevels.filter(level => level.isLevelSafe).length
   const solution = numberOfSafeLevels
 
-  await write(fromHere('output.txt'), levelsExpanded.map(level => `${level.original} => ${level.isLevelSafe ? 'SAFE' : 'UNSAFE'}`).join('\n'))
+  await write(fromHere('output.txt'), scoredLevels.map(level => `${level.original} => ${level.isLevelSafe ? 'SAFE' : 'UNSAFE'}`).join('\n'))
 
-  report('Expanded:', levelsExpanded)
+  report('Expanded:', scoredLevels)
   report('Solution 1:', solution)
 }
 
+function scoreLevel (level) {
+  const original = [...level]
+  const sorted = level.sort((a, b) => a - b)
+  const isAscending = sorted.join('') === original.join('')
+  const isDescending = sorted.slice().reverse().join('') === original.join('')
+  const maxDiff = original.reduce((acc, value, index) => {
+    const nextValue = original[index + 1]
+    if (nextValue === undefined) return acc
+    const diff = Math.abs(value - nextValue)
+    return diff > acc ? diff : acc
+  }, 0)
+  const minDiff = original.reduce((acc, value, index) => {
+    const nextValue = original[index + 1]
+    if (nextValue === undefined) return acc
+    const diff = Math.abs(nextValue - value)
+    return diff < acc ? diff : acc
+  }, Infinity)
+
+  const isDiffSafe = maxDiff <= 3 && minDiff >= 1
+  const isLevelSafe = (isAscending || isDescending) && isDiffSafe
+  return {
+    original: original.join(' '),
+    sorted: sorted.join(' '),
+    reverse: sorted.slice().reverse().join(' '),
+    isAscending,
+    isDescending,
+    minDiff,
+    maxDiff,
+    isDiffSafe,
+    isLevelSafe
+  }
+}
+
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const lines = input.split('\n')
+  const levels = lines.map(line => line.trim().split(' ').map(Number))
+
+  const expandedlevels = []
+  levels.forEach(level => {
+    // Remove one element from the level, and check if it's safe
+    for (let i = 0; i < level.length; i++) {
+      const modifiedLevel = [...level]
+      modifiedLevel.splice(i, 1)
+      expandedlevels.push({ level, modifiedLevel })
+    }
+  })
+
+  const scoredLevels = expandedlevels.map(({ level, modifiedLevel }) => {
+    const score = scoreLevel(modifiedLevel)
+    return {
+      ...score,
+      original: level.join(' '),
+      modified: modifiedLevel.join(' ')
+    }
+  })
+  const indexOfSafeLevels = scoredLevels.reduce((acc, level, index) => {
+    const isSafe = level.isLevelSafe
+    const existing = acc[level.original] ?? 0
+    acc[level.original] = isSafe ? existing + 1 : existing
+    return acc
+  }, {})
+
+  const numberOfSafeLevels = Object.values(indexOfSafeLevels).filter(count => count >= 1).length
+  const solution = numberOfSafeLevels
+
   report('Solution 2:', solution)
 }
 
