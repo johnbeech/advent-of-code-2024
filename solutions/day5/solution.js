3f517b712ca4db9a858b90079eb3594628c4d815
John Beech <johnbeech.github@mkv25.net>
2024-12-05 08:05:00 +0000

    Solve day5 for second star

--- solutions/day5/solution.js
+++ solutions/day5/solution.js
@@ -24,16 +24,23 @@ function validatePage (page, rules) {
   })
 }
 
-async function solveForFirstStar (input) {
+function parseInput (input) {
   const [rulesInput, pagesInput] = input.split('\n\n')
+
   const rules = rulesInput.split('\n').map(rule => {
     const [L, R] = rule.split('|').map(Number)
     return { L, R }
   })
+
   const pageUpdates = pagesInput.split('\n').map(line => {
     return line.split(',').map(Number)
   })
 
+  return { rules, pageUpdates }
+}
+
+async function solveForFirstStar (input) {
+  const { rules, pageUpdates } = parseInput(input)
   report('Rules:', rules)
   report('Pages:', pageUpdates)
 
@@ -41,7 +48,7 @@ async function solveForFirstStar (input) {
     return validatePage(page, rules)
   })
 
-  report('Valid pages:', validPageUpdates)
+  report('Valid pages:', validPageUpdates.length)
 
   const middleNumberFromValidPages = validPageUpdates.map(page => page[page.length / 2 | 0])
   const sumOfMiddlePageNumbers = middleNumberFromValidPages.reduce((sum, number) => sum + number, 0)
@@ -50,8 +57,53 @@ async function solveForFirstStar (input) {
   report('Solution 1:', solution)
 }
 
+function fixPageBasedOnRules (page, rules) {
+  // Fix the page based on the rules
+  // Page A must exist before Page B if present
+  const fixedPage = page.slice()
+  let changed = true
+  console.log('Fixing page...', page.join(','))
+  while (changed) {
+    changed = false
+    console.log('  Looping...', fixedPage.join(','))
+    rules.forEach(rule => {
+      const { L, R } = rule
+      const pageA = fixedPage.indexOf(L)
+      const pageB = fixedPage.indexOf(R)
+      if (pageA === -1 || pageB === -1) {
+        return
+      }
+      if (pageA > pageB) {
+        fixedPage[pageA] = R
+        fixedPage[pageB] = L
+        changed = true
+      }
+    })
+  }
+  return fixedPage
+}
+
 async function solveForSecondStar (input) {
-  const solution = 'UNSOLVED'
+  const { rules, pageUpdates } = parseInput(input)
+
+  const invalidPageUpdates = pageUpdates.filter(page => {
+    return !validatePage(page, rules)
+  })
+
+  console.log('')
+  report('Second Star')
+  report('Invalid pages:', invalidPageUpdates.length)
+
+  const fixedPages = invalidPageUpdates.map(page => {
+    return fixPageBasedOnRules(page, rules)
+  })
+
+  report('Fixed pages:', fixedPages.length)
+
+  const middleNumberFromValidPages = fixedPages.map(page => page[page.length / 2 | 0])
+  const sumOfMiddlePageNumbers = middleNumberFromValidPages.reduce((sum, number) => sum + number, 0)
+
+  const solution = sumOfMiddlePageNumbers
   report('Solution 2:', solution)
 }
 
