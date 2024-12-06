90e0acc72b69d682d2d3516f6ec2df832ea1936b
John Beech <johnbeech.github@mkv25.net>
2024-12-06 06:50:04 +0000

    Solve day6 for first star

---
+++ solutions/day6/solution.js
@@ -0,0 +1,79 @@
+const path = require('path')
+const { read, position } = require('promise-path')
+const fromHere = position(__dirname)
+const report = (...messages) => console.log(`[${require(fromHere('../../package.json')).logName} / ${__dirname.split(path.sep).pop()}]`, ...messages)
+
+async function run () {
+  const input = (await read(fromHere('input.txt'), 'utf8')).trim()
+
+  await solveForFirstStar(input)
+  await solveForSecondStar(input)
+}
+
+const directions = [
+  { x: 0, y: -1 }, // North
+  { x: 1, y: 0 }, // East
+  { x: 0, y: 1 }, // South
+  { x: -1, y: 0 } // West
+]
+
+async function solveForFirstStar (input) {
+  const grid = input.split('\n').map(line => line.split(''))
+  const map = grid.reduce((map, line, y) => {
+    line.forEach((char, x) => {
+      map.set(`${x},${y}`, { char, x, y })
+    })
+    return map
+  }, new Map())
+
+  // Link all points to their neighbors
+  map.forEach(point => {
+    point.neighbors = directions.map(({ x, y }) => map.get(`${point.x + x},${point.y + y}`))
+  })
+
+  const mapWidth = grid[0].length
+  const mapHeight = grid.length
+
+  // Find guard ^ facing north
+  const guardStartLocation = [...map.values()].find(point => point.char === '^')
+  const guard = {
+    direction: 0,
+    ...guardStartLocation
+  }
+  guardStartLocation.visited = true
+
+  console.log('Guard:', guard)
+
+  while (guard.x >= 0 && guard.x < mapWidth && guard.y >= 0 && guard.y < mapHeight) {
+    const next = guard.neighbors[guard.direction]
+    if (!next) {
+      report('Guard exited the map from', guard.x, guard.y)
+      break
+    } else if (next.char === '#') {
+      // Turn right
+      guard.direction = (guard.direction + 1) % 4
+      const newDirectionName = ['North', 'East', 'South', 'West'][guard.direction]
+      console.log('Turning right to face', newDirectionName)
+    } else {
+      // Move forward
+      guard.x = next.x
+      guard.y = next.y
+      guard.neighbors = next.neighbors
+      next.visited = true
+      console.log('Moving forward to', next.x, next.y)
+    }
+  }
+
+  // Count all visited locations
+  const visitedLocations = [...map.values()].filter(location => location.visited)
+
+  const solution = visitedLocations.length
+  report('Solution 1:', solution)
+}
+
+async function solveForSecondStar (input) {
+  const solution = 'UNSOLVED'
+  report('Solution 2:', solution)
+}
+
+run()
